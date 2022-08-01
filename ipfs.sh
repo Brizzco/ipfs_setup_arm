48a75f42f0d3ece4d749b8c68d4c2943bec9b415
Brizzco <86152539+Brizzco@users.noreply.github.com>
2022-08-01 21:24:31 +0200

    Update ipfs.sh

--- ipfs.sh
+++ ipfs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-wget https://dist.ipfs.io/kubo/v0.14.0/kubo_v0.14.0_linux-amd64.tar.gz && 
-tar -xvzf kubo_v0.14.0_linux-amd64.tar.gz && 
+wget https://dist.ipfs.io/kubo/v0.14.0/kubo_v0.14.0_linux-arm64.tar.gz && 
+tar -xvzf kubo_v0.14.0_linux-arm64.tar.gz && 
 cd kubo && 
 sudo bash install.sh && 
 cd .. && 
